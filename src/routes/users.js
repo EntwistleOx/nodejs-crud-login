ad81fbbb5f0fefac7174726c250c235022260b0d
Juan Díaz <juan.diaz.12069@gmail.com>
2019-08-12 17:02:54 -0400

    Added User model, and created password functions for encrypt

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,4 +9,29 @@ router.get('/users/signup', (req, res) => {
     res.render('users/signup');
 });
 
+router.post('/users/store', (req, res) => {
+    const {name, email, password, confirm_password} = req.body;
+    const errors = [];
+    if(!name) {
+        errors.push({text: 'Please write your email!'});
+    }
+    if(!email) {
+        errors.push({text: 'Please write your name!'});
+    }
+    if(password != confirm_password) {
+        errors.push({text: 'Passwords no dot match =('});
+    }
+    if(password.length < 4) {
+        errors.push({text: 'Passwords must be at least 4 chatacters ;)'});
+    }
+    if(errors.length > 0) {
+        res.render('users/signup', {
+            errors,
+            name,
+            email
+        });
+    }
+    res.send('ok');
+})
+
 module.exports = router;
