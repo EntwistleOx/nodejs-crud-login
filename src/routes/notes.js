404b3a26eff307c6ea441094308b65513d1da6ab
Juan Díaz <juan.diaz.12069@gmail.com>
2019-08-12 14:11:46 -0400

    Added footer, Note model, notes index, notes create, notes save

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Note = require('../models/Note');
 
-router.get('/notes', (req, res) => {
-    res.send('Notes ok');
+router.get('/notes/create', (req, res) => {
+    res.render('notes/create');
 });
+
+router.post('/notes/store', async (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if(!title) {
+        errors.push({text: 'Please write a title'});
+    }
+    if(!description) {
+        errors.push({text: 'Please write a description'});
+    }
+
+    if(errors.length > 0) {
+        res.render('notes/create', {
+            errors,
+            title,
+            description
+        });
+    }else {
+        const newNote = new Note({title, description });
+        await newNote.save();
+        res.redirect('/notes');
+    }
+});
+
+router.get('/notes', async (req, res) => {
+    const notes = await Note.find().sort({date: 'desc'});
+    res.render('notes/index', { notes });
+});
+
+router.get('/notes/edit')
 
 module.exports = router;
