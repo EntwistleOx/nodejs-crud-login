22ceabf94bd0f7aa9637b3f817d51b3939d41a98
Juan Díaz <juan.diaz.12069@gmail.com>
2019-08-12 10:29:38 -0400

    Added basic configurations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
-
-//Server Init
+const path = require('path');
+const handlebars = require('express-handlebars');
+const method = require('method-override');
+const session = require('express-session');
 
 //Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', handlebars({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares
+app.use(express.urlencoded({extended:false}));
+app.use(method('_method'));
+app.use(session({
+    secret: myApp,
+    resave: true,
+    saveUninitialized: true
+}));
 
-//Globañl Vars
+//Global Vars
 
 //Routes
 
 //Static Files
+
+//Server is listening
+app.listen(app.get('port'), () => {
+    console.log('Server on port: ', app.get('port'));
+});
