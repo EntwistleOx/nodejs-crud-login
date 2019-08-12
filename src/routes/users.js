68fc063e273a2b2438631c3a7fb9920b1bd715ff
Juan Díaz <juan.diaz.12069@gmail.com>
2019-08-12 11:41:29 -0400

    added main template, bootstrap4, and index / about pages

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/users/signin', (req, res) => {
-    res.send('SignIn ok');
+    res.render('users/signin');
 });
 
 router.get('/users/signup', (req, res) => {
-    res.send('SignUp ok');
+    res.render('users/signup');
 });
 
 module.exports = router;
